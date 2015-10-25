34fe069f14ed3d1451408e4a2538a9f531fd063d
mhj <mahyunju@mahyunju.local>
2015-10-25 10:24:46 +0000

    button

---
+++ Main_menu/Assets/signup_bt.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class signup_bt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void gotoSignup(){
+		//
+		Application.LoadLevel ("sign_up");
+	}
+}
