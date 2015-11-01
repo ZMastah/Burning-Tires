a4f1ab86a5e9166e49aa9c339a5368fd7bcd5c1e
ZMastah <jesse.inkeroinen@hotmail.com>
2015-11-01 20:29:32 +0900

    Lot of visual changes and triggers.
    Models, mapping, effects, prefabs etc.

---
+++ Burning Tires/Assets/Sky Loop Map/Scripts/CheckpointTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointTrigger : MonoBehaviour {
+
+    public Material greenMat;
+    public Material redMat;
+    public Material blackMat;
+    private GameObject[] allCheckpoints;
+	void Start () {
+        allCheckpoints = GameObject.FindGameObjectsWithTag("CheckPoint");
+	}
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if(coll.gameObject.name == "CheckpointPanel")
+        {
+            for (var i = 0; i < allCheckpoints.Length; i++)
+            {
+                allCheckpoints[i].transform.GetChild(1).gameObject.GetComponent<MeshRenderer>().material = redMat;
+                allCheckpoints[i].transform.GetChild(2).gameObject.GetComponent<MeshRenderer>().material = blackMat;
+            }
+            coll.transform.GetChild(1).gameObject.GetComponent<MeshRenderer>().material = blackMat;
+            coll.transform.GetChild(2).gameObject.GetComponent<MeshRenderer>().material = greenMat;
+        }
+    }
+}
